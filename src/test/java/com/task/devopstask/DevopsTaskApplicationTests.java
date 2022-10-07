d76f02e827926d0af5fc80bc5250071bcd20b193
Aadil Sajjad <aadilsajjad@NIS-PK686.local>
2022-10-07 16:32:26 +0500

    first commit on devops task

---
+++ src/test/java/com/task/devopstask/DevopsTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.devopstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
