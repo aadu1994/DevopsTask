0dc9558ac3351b5d2fdb2ff3043126152bd71e9e
Aadil Sajjad <aadilsajjad@NIS-PK686.local>
2022-10-10 12:59:56 +0500

    added new new output new

--- src/main/java/com/task/devopstask/DevopsTaskApplication.java
+++ src/main/java/com/task/devopstask/DevopsTaskApplication.java
@@ -14,7 +14,7 @@ public class DevopsTaskApplication {
     }
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World Aadil bla bla bla, new update") String name) {
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World Aadil bla bla bla, new update, new update 2") String name) {
         return String.format("Hello %s!", name);
     }
 
